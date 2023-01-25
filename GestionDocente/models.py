65aa1914a503e6b7abe9b153bb759ec4ed0fa93a
JaimeCastro0926 <45641739+JaimeCastro0926@users.noreply.github.com>
2023-01-25 11:45:31 -0500

    Update models.py

--- GestionDocente/models.py
+++ GestionDocente/models.py
@@ -144,7 +144,7 @@ class Convivencia(models.Model):
     Puntos_negativos=models.PositiveIntegerField(default=1)
 
 class Psicoorientacion(models.Model):
-    psicoorientador = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='updated_posts', on_delete=models.CASCADE)
+    psicoorientador = psicoorientador = User.objects.get(username = Self.request.user)
     Estudiante=models.ForeignKey(Estudiante,on_delete=models.CASCADE)
     Fecha=models.DateField(auto_now_add=True, auto_now=False)
     Hora = models.TimeField(auto_now_add=True, auto_now=False)
